394a1f739d5ba55f4613a1bff22dbb784edbcc19
qmuntal <quimmuntal@gmail.com>
2019-02-20 17:20:33 +0100

    add benchmarks for Open

---
+++ bench_test.go
@@ -0,0 +1,72 @@
+package gltf
+
+import "testing"
+
+func BenchmarkOpenASCII(b *testing.B) {
+	benchs := []struct {
+		name string
+	}{
+		{"testdata/AnimatedCube/glTF/AnimatedCube.gltf"},
+		{"testdata/BoxVertexColors/glTF/BoxVertexColors.gltf"},
+		{"testdata/Cameras/glTF/Cameras.gltf"},
+		{"testdata/Cube/glTF/Cube.gltf"},
+		{"testdata/EnvironmentTest/glTF/EnvironmentTest.gltf"},
+		{"testdata/OrientationTest/glTF/OrientationTest.gltf"},
+		{"testdata/Triangle/glTF/Triangle.gltf"},
+		{"testdata/TriangleWithoutIndices/glTF/TriangleWithoutIndices.gltf"},
+	}
+	for _, bb := range benchs {
+		b.Run(bb.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, err := Open(bb.name)
+				if err != nil {
+					b.Errorf("Open() error = %v", err)
+					return
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkOpenEmbedded(b *testing.B) {
+	benchs := []struct {
+		name string
+	}{
+		{"testdata/BoxVertexColors/glTF-Embedded/BoxVertexColors.gltf"},
+		{"testdata/Cameras/glTF-Embedded/Cameras.gltf"},
+		{"testdata/OrientationTest/glTF-Embedded/OrientationTest.gltf"},
+		{"testdata/Triangle/glTF-Embedded/Triangle.gltf"},
+		{"testdata/TriangleWithoutIndices/glTF-Embedded/TriangleWithoutIndices.gltf"},
+	}
+	for _, bb := range benchs {
+		b.Run(bb.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, err := Open(bb.name)
+				if err != nil {
+					b.Errorf("Open() error = %v", err)
+					return
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkOpenBinary(b *testing.B) {
+	benchs := []struct {
+		name string
+	}{
+		{"testdata/BoxVertexColors/glTF-Binary/BoxVertexColors.glb"},
+		{"testdata/OrientationTest/glTF-Binary/OrientationTest.glb"},
+	}
+	for _, bb := range benchs {
+		b.Run(bb.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, err := Open(bb.name)
+				if err != nil {
+					b.Errorf("Open() error = %v", err)
+					return
+				}
+			}
+		})
+	}
+}
