8a84042af298187154b9318ca7b5174b3f2caf1e
Quim Muntal <quimmuntal@gmail.com>
2019-11-10 11:55:10 +0100

    add tests for unlit

--- unlit/unlit_test.go
+++ unlit/unlit_test.go
@@ -35,3 +35,29 @@ func TestUnlit_UnmarshalJSON(t *testing.T) {
 		})
 	}
 }
+
+func TestUnmarshal(t *testing.T) {
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    interface{}
+		wantErr bool
+	}{
+		{"base", args{[]byte("{}")}, &Unlit{}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Unmarshal(tt.args.data)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Unmarshal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Unmarshal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
