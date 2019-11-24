759f59cad16bd8d669b8d0d399fb976b7281a573
Quim Muntal <quimmuntal@gmail.com>
2019-11-24 14:02:43 +0100

    add modeler tests

--- modeler/modeler_test.go
+++ modeler/modeler_test.go
@@ -152,3 +152,207 @@ func TestModeler_AddPosition(t *testing.T) {
 		})
 	}
 }
+
+func TestModeler_AddJoints(t *testing.T) {
+	type args struct {
+		bufferIndex uint32
+		data        interface{}
+	}
+	tests := []struct {
+		name    string
+		m       *Modeler
+		args    args
+		want    uint32
+		wantDoc *gltf.Document
+	}{
+		{"uint8", &Modeler{Document: &gltf.Document{
+			Accessors: []gltf.Accessor{{}},
+			Buffers:   []gltf.Buffer{{ByteLength: 10}},
+		}}, args{0, [][4]uint8{{1, 2, 3, 4}}}, 1, &gltf.Document{
+			Accessors: []gltf.Accessor{
+				{},
+				{BufferView: gltf.Index(0), Count: 1, Type: gltf.Vec4, ComponentType: gltf.UnsignedByte},
+			},
+			BufferViews: []gltf.BufferView{
+				{ByteLength: 4, Target: gltf.ArrayBuffer},
+			},
+			Buffers: []gltf.Buffer{
+				{ByteLength: 14, Data: []byte{1, 2, 3, 4}},
+			},
+		}},
+		{"uint16", &Modeler{Document: &gltf.Document{
+			Accessors: []gltf.Accessor{{}},
+			Buffers:   []gltf.Buffer{{ByteLength: 10}},
+		}}, args{0, [][4]uint16{{1, 2, 3, 4}}}, 1, &gltf.Document{
+			Accessors: []gltf.Accessor{
+				{},
+				{BufferView: gltf.Index(0), Count: 1, Type: gltf.Vec4, ComponentType: gltf.UnsignedShort},
+			},
+			BufferViews: []gltf.BufferView{
+				{ByteLength: 8, Target: gltf.ArrayBuffer},
+			},
+			Buffers: []gltf.Buffer{
+				{ByteLength: 18, Data: []byte{1, 0, 2, 0, 3, 0, 4, 0}},
+			},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.AddJoints(tt.args.bufferIndex, tt.args.data)
+			if tt.want != got {
+				t.Errorf("Modeler.AddJoints() = %v, want %v", got, tt.want)
+				return
+			}
+			if diff := deep.Equal(tt.m.Document, tt.wantDoc); diff != nil {
+				t.Errorf("Modeler.AddJoints() = %v", diff)
+				return
+			}
+		})
+	}
+}
+
+func TestModeler_AddWeights(t *testing.T) {
+	type args struct {
+		bufferIndex uint32
+		data        interface{}
+	}
+	tests := []struct {
+		name    string
+		m       *Modeler
+		args    args
+		want    uint32
+		wantDoc *gltf.Document
+	}{
+		{"uint8", &Modeler{Document: &gltf.Document{
+			Accessors: []gltf.Accessor{{}},
+			Buffers:   []gltf.Buffer{{ByteLength: 10}},
+		}}, args{0, [][4]uint8{{1, 2, 3, 4}}}, 1, &gltf.Document{
+			Accessors: []gltf.Accessor{
+				{},
+				{BufferView: gltf.Index(0), Count: 1, Type: gltf.Vec4, ComponentType: gltf.UnsignedByte, Normalized: true},
+			},
+			BufferViews: []gltf.BufferView{
+				{ByteLength: 4, Target: gltf.ArrayBuffer},
+			},
+			Buffers: []gltf.Buffer{
+				{ByteLength: 14, Data: []byte{1, 2, 3, 4}},
+			},
+		}},
+		{"uint16", &Modeler{Document: &gltf.Document{
+			Accessors: []gltf.Accessor{{}},
+			Buffers:   []gltf.Buffer{{ByteLength: 10}},
+		}}, args{0, [][4]uint16{{1, 2, 3, 4}}}, 1, &gltf.Document{
+			Accessors: []gltf.Accessor{
+				{},
+				{BufferView: gltf.Index(0), Count: 1, Type: gltf.Vec4, ComponentType: gltf.UnsignedShort, Normalized: true},
+			},
+			BufferViews: []gltf.BufferView{
+				{ByteLength: 8, Target: gltf.ArrayBuffer},
+			},
+			Buffers: []gltf.Buffer{
+				{ByteLength: 18, Data: []byte{1, 0, 2, 0, 3, 0, 4, 0}},
+			},
+		}},
+		{"float", &Modeler{Document: &gltf.Document{
+			Accessors: []gltf.Accessor{{}},
+			Buffers:   []gltf.Buffer{{ByteLength: 10}},
+		}}, args{0, [][4]float32{{1, 2, 3, 4}, {}}}, 1, &gltf.Document{
+			Accessors: []gltf.Accessor{
+				{},
+				{BufferView: gltf.Index(0), Count: 2, Type: gltf.Vec4, ComponentType: gltf.Float},
+			},
+			BufferViews: []gltf.BufferView{
+				{ByteLength: 32, Target: gltf.ArrayBuffer},
+			},
+			Buffers: []gltf.Buffer{
+				{ByteLength: 42, Data: []byte{0, 0, 128, 63, 0, 0, 0, 64, 0, 0, 64, 64, 0, 0, 128, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+			},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.AddWeights(tt.args.bufferIndex, tt.args.data)
+			if tt.want != got {
+				t.Errorf("Modeler.AddWeights() = %v, want %v", got, tt.want)
+				return
+			}
+			if diff := deep.Equal(tt.m.Document, tt.wantDoc); diff != nil {
+				t.Errorf("Modeler.AddWeights() = %v", diff)
+				return
+			}
+		})
+	}
+}
+
+func TestModeler_AddTextureCoord(t *testing.T) {
+	type args struct {
+		bufferIndex uint32
+		data        interface{}
+	}
+	tests := []struct {
+		name    string
+		m       *Modeler
+		args    args
+		want    uint32
+		wantDoc *gltf.Document
+	}{
+		{"uint8", &Modeler{Document: &gltf.Document{
+			Accessors: []gltf.Accessor{{}},
+			Buffers:   []gltf.Buffer{{ByteLength: 10}},
+		}}, args{0, [][2]uint8{{1, 2}}}, 1, &gltf.Document{
+			Accessors: []gltf.Accessor{
+				{},
+				{BufferView: gltf.Index(0), Count: 1, Type: gltf.Vec2, ComponentType: gltf.UnsignedByte, Normalized: true},
+			},
+			BufferViews: []gltf.BufferView{
+				{ByteLength: 2, Target: gltf.ArrayBuffer},
+			},
+			Buffers: []gltf.Buffer{
+				{ByteLength: 12, Data: []byte{1, 2}},
+			},
+		}},
+		{"uint16", &Modeler{Document: &gltf.Document{
+			Accessors: []gltf.Accessor{{}},
+			Buffers:   []gltf.Buffer{{ByteLength: 10}},
+		}}, args{0, [][2]uint16{{1, 2}}}, 1, &gltf.Document{
+			Accessors: []gltf.Accessor{
+				{},
+				{BufferView: gltf.Index(0), Count: 1, Type: gltf.Vec2, ComponentType: gltf.UnsignedShort, Normalized: true},
+			},
+			BufferViews: []gltf.BufferView{
+				{ByteLength: 4, Target: gltf.ArrayBuffer},
+			},
+			Buffers: []gltf.Buffer{
+				{ByteLength: 14, Data: []byte{1, 0, 2, 0}},
+			},
+		}},
+		{"float", &Modeler{Document: &gltf.Document{
+			Accessors: []gltf.Accessor{{}},
+			Buffers:   []gltf.Buffer{{ByteLength: 10}},
+		}}, args{0, [][2]float32{{1, 2}, {}}}, 1, &gltf.Document{
+			Accessors: []gltf.Accessor{
+				{},
+				{BufferView: gltf.Index(0), Count: 2, Type: gltf.Vec2, ComponentType: gltf.Float},
+			},
+			BufferViews: []gltf.BufferView{
+				{ByteLength: 16, Target: gltf.ArrayBuffer},
+			},
+			Buffers: []gltf.Buffer{
+				{ByteLength: 26, Data: []byte{0, 0, 128, 63, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0}},
+			},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.AddTextureCoord(tt.args.bufferIndex, tt.args.data)
+			if tt.want != got {
+				t.Errorf("Modeler.AddTextureCoord() = %v, want %v", got, tt.want)
+				return
+			}
+			if diff := deep.Equal(tt.m.Document, tt.wantDoc); diff != nil {
+				t.Errorf("Modeler.AddTextureCoord() = %v", diff)
+				return
+			}
+		})
+	}
+}
