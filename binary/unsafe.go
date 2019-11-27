23d710b3945487f0ef194e96053d5a4e9b045c09
Quim Muntal <quimmuntal@gmail.com>
2019-11-27 20:39:26 +0100

    replace binary

--- binary/unsafe.go
+++ binary/unsafe.go
@@ -9,190 +9,190 @@ type shortComponent struct{}
 type floatComponent struct{}
 
 func (byteComponent) Scalar(b []byte) int8 {
-	v := UnsignedByte.Scalar(b)
+	v := Ubyte.Scalar(b)
 	return *(*int8)(unsafe.Pointer(&v))
 }
 
 func (byteComponent) PutScalar(b []byte, v int8) {
-	UnsignedByte.PutScalar(b, *(*uint8)(unsafe.Pointer(&v)))
+	Ubyte.PutScalar(b, *(*uint8)(unsafe.Pointer(&v)))
 }
 
 func (byteComponent) Vec2(b []byte) [2]int8 {
-	v := UnsignedByte.Vec2(b)
+	v := Ubyte.Vec2(b)
 	return *(*[2]int8)(unsafe.Pointer(&v))
 }
 
 func (byteComponent) PutVec2(b []byte, v [2]int8) {
-	UnsignedByte.PutVec2(b, *(*[2]uint8)(unsafe.Pointer(&v)))
+	Ubyte.PutVec2(b, *(*[2]uint8)(unsafe.Pointer(&v)))
 }
 
 func (byteComponent) Vec3(b []byte) [3]int8 {
-	v := UnsignedByte.Vec3(b)
+	v := Ubyte.Vec3(b)
 	return *(*[3]int8)(unsafe.Pointer(&v))
 }
 
 func (byteComponent) PutVec3(b []byte, v [3]int8) {
-	UnsignedByte.PutVec3(b, *(*[3]uint8)(unsafe.Pointer(&v)))
+	Ubyte.PutVec3(b, *(*[3]uint8)(unsafe.Pointer(&v)))
 }
 
 func (byteComponent) Vec4(b []byte) [4]int8 {
-	v := UnsignedByte.Vec4(b)
+	v := Ubyte.Vec4(b)
 	return *(*[4]int8)(unsafe.Pointer(&v))
 }
 
 func (byteComponent) PutVec4(b []byte, v [4]int8) {
-	UnsignedByte.PutVec4(b, *(*[4]uint8)(unsafe.Pointer(&v)))
+	Ubyte.PutVec4(b, *(*[4]uint8)(unsafe.Pointer(&v)))
 }
 
 func (byteComponent) Mat2(b []byte) [2][2]int8 {
-	v := UnsignedByte.Mat2(b)
+	v := Ubyte.Mat2(b)
 	return *(*[2][2]int8)(unsafe.Pointer(&v))
 }
 
 func (byteComponent) PutMat2(b []byte, v [2][2]int8) {
-	UnsignedByte.PutMat2(b, *(*[2][2]uint8)(unsafe.Pointer(&v)))
+	Ubyte.PutMat2(b, *(*[2][2]uint8)(unsafe.Pointer(&v)))
 }
 
 func (byteComponent) Mat3(b []byte) [3][3]int8 {
-	v := UnsignedByte.Mat3(b)
+	v := Ubyte.Mat3(b)
 	return *(*[3][3]int8)(unsafe.Pointer(&v))
 }
 
 func (byteComponent) PutMat3(b []byte, v [3][3]int8) {
-	UnsignedByte.PutMat3(b, *(*[3][3]uint8)(unsafe.Pointer(&v)))
+	Ubyte.PutMat3(b, *(*[3][3]uint8)(unsafe.Pointer(&v)))
 }
 
 func (byteComponent) Mat4(b []byte) [4][4]int8 {
-	v := UnsignedByte.Mat4(b)
+	v := Ubyte.Mat4(b)
 	return *(*[4][4]int8)(unsafe.Pointer(&v))
 }
 
 func (byteComponent) PutMat4(b []byte, v [4][4]int8) {
-	UnsignedByte.PutMat4(b, *(*[4][4]uint8)(unsafe.Pointer(&v)))
+	Ubyte.PutMat4(b, *(*[4][4]uint8)(unsafe.Pointer(&v)))
 }
 
 func (shortComponent) Scalar(b []byte) int16 {
-	v := UnsignedShort.Scalar(b)
+	v := Ushort.Scalar(b)
 	return *(*int16)(unsafe.Pointer(&v))
 }
 
 func (shortComponent) PutScalar(b []byte, v int16) {
-	UnsignedShort.PutScalar(b, *(*uint16)(unsafe.Pointer(&v)))
+	Ushort.PutScalar(b, *(*uint16)(unsafe.Pointer(&v)))
 }
 
 func (shortComponent) Vec2(b []byte) [2]int16 {
-	v := UnsignedShort.Vec2(b)
+	v := Ushort.Vec2(b)
 	return *(*[2]int16)(unsafe.Pointer(&v))
 }
 
 func (shortComponent) PutVec2(b []byte, v [2]int16) {
-	UnsignedShort.PutVec2(b, *(*[2]uint16)(unsafe.Pointer(&v)))
+	Ushort.PutVec2(b, *(*[2]uint16)(unsafe.Pointer(&v)))
 }
 
 func (shortComponent) Vec3(b []byte) [3]int16 {
-	v := UnsignedShort.Vec3(b)
+	v := Ushort.Vec3(b)
 	return *(*[3]int16)(unsafe.Pointer(&v))
 }
 
 func (shortComponent) PutVec3(b []byte, v [3]int16) {
-	UnsignedShort.PutVec3(b, *(*[3]uint16)(unsafe.Pointer(&v)))
+	Ushort.PutVec3(b, *(*[3]uint16)(unsafe.Pointer(&v)))
 }
 
 func (shortComponent) Vec4(b []byte) [4]int16 {
-	v := UnsignedShort.Vec4(b)
+	v := Ushort.Vec4(b)
 	return *(*[4]int16)(unsafe.Pointer(&v))
 }
 
 func (shortComponent) PutVec4(b []byte, v [4]int16) {
-	UnsignedShort.PutVec4(b, *(*[4]uint16)(unsafe.Pointer(&v)))
+	Ushort.PutVec4(b, *(*[4]uint16)(unsafe.Pointer(&v)))
 }
 
 func (shortComponent) Mat2(b []byte) [2][2]int16 {
-	v := UnsignedShort.Mat2(b)
+	v := Ushort.Mat2(b)
 	return *(*[2][2]int16)(unsafe.Pointer(&v))
 }
 
 func (shortComponent) PutMat2(b []byte, v [2][2]int16) {
-	UnsignedShort.PutMat2(b, *(*[2][2]uint16)(unsafe.Pointer(&v)))
+	Ushort.PutMat2(b, *(*[2][2]uint16)(unsafe.Pointer(&v)))
 }
 
 func (shortComponent) Mat3(b []byte) [3][3]int16 {
-	v := UnsignedShort.Mat3(b)
+	v := Ushort.Mat3(b)
 	return *(*[3][3]int16)(unsafe.Pointer(&v))
 }
 
 func (shortComponent) PutMat3(b []byte, v [3][3]int16) {
-	UnsignedShort.PutMat3(b, *(*[3][3]uint16)(unsafe.Pointer(&v)))
+	Ushort.PutMat3(b, *(*[3][3]uint16)(unsafe.Pointer(&v)))
 }
 
 func (shortComponent) Mat4(b []byte) [4][4]int16 {
-	v := UnsignedShort.Mat4(b)
+	v := Ushort.Mat4(b)
 	return *(*[4][4]int16)(unsafe.Pointer(&v))
 }
 
 func (shortComponent) PutMat4(b []byte, v [4][4]int16) {
-	UnsignedShort.PutMat4(b, *(*[4][4]uint16)(unsafe.Pointer(&v)))
+	Ushort.PutMat4(b, *(*[4][4]uint16)(unsafe.Pointer(&v)))
 }
 
 func (floatComponent) Scalar(b []byte) float32 {
-	v := UnsignedInt.Scalar(b)
+	v := UInt.Scalar(b)
 	return *(*float32)(unsafe.Pointer(&v))
 }
 
 func (floatComponent) PutScalar(b []byte, v float32) {
-	UnsignedInt.PutScalar(b, *(*uint32)(unsafe.Pointer(&v)))
+	UInt.PutScalar(b, *(*uint32)(unsafe.Pointer(&v)))
 }
 
 func (floatComponent) Vec2(b []byte) [2]float32 {
-	v := UnsignedInt.Vec2(b)
+	v := UInt.Vec2(b)
 	return *(*[2]float32)(unsafe.Pointer(&v))
 }
 
 func (floatComponent) PutVec2(b []byte, v [2]float32) {
-	UnsignedInt.PutVec2(b, *(*[2]uint32)(unsafe.Pointer(&v)))
+	UInt.PutVec2(b, *(*[2]uint32)(unsafe.Pointer(&v)))
 }
 
 func (floatComponent) Vec3(b []byte) [3]float32 {
-	v := UnsignedInt.Vec3(b)
+	v := UInt.Vec3(b)
 	return *(*[3]float32)(unsafe.Pointer(&v))
 }
 
 func (floatComponent) PutVec3(b []byte, v [3]float32) {
-	UnsignedInt.PutVec3(b, *(*[3]uint32)(unsafe.Pointer(&v)))
+	UInt.PutVec3(b, *(*[3]uint32)(unsafe.Pointer(&v)))
 }
 
 func (floatComponent) Vec4(b []byte) [4]float32 {
-	v := UnsignedInt.Vec4(b)
+	v := UInt.Vec4(b)
 	return *(*[4]float32)(unsafe.Pointer(&v))
 }
 
 func (floatComponent) PutVec4(b []byte, v [4]float32) {
-	UnsignedInt.PutVec4(b, *(*[4]uint32)(unsafe.Pointer(&v)))
+	UInt.PutVec4(b, *(*[4]uint32)(unsafe.Pointer(&v)))
 }
 
 func (floatComponent) Mat2(b []byte) [2][2]float32 {
-	v := UnsignedInt.Mat2(b)
+	v := UInt.Mat2(b)
 	return *(*[2][2]float32)(unsafe.Pointer(&v))
 }
 
 func (floatComponent) PutMat2(b []byte, v [2][2]float32) {
-	UnsignedInt.PutMat2(b, *(*[2][2]uint32)(unsafe.Pointer(&v)))
+	UInt.PutMat2(b, *(*[2][2]uint32)(unsafe.Pointer(&v)))
 }
 
 func (floatComponent) Mat3(b []byte) [3][3]float32 {
-	v := UnsignedInt.Mat3(b)
+	v := UInt.Mat3(b)
 	return *(*[3][3]float32)(unsafe.Pointer(&v))
 }
 
 func (floatComponent) PutMat3(b []byte, v [3][3]float32) {
-	UnsignedInt.PutMat3(b, *(*[3][3]uint32)(unsafe.Pointer(&v)))
+	UInt.PutMat3(b, *(*[3][3]uint32)(unsafe.Pointer(&v)))
 }
 
 func (floatComponent) Mat4(b []byte) [4][4]float32 {
-	v := UnsignedInt.Mat4(b)
+	v := UInt.Mat4(b)
 	return *(*[4][4]float32)(unsafe.Pointer(&v))
 }
 
 func (floatComponent) PutMat4(b []byte, v [4][4]float32) {
-	UnsignedInt.PutMat4(b, *(*[4][4]uint32)(unsafe.Pointer(&v)))
+	UInt.PutMat4(b, *(*[4][4]uint32)(unsafe.Pointer(&v)))
 }
